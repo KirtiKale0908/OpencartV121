2c5399c67862fa6faae5d4d54a05976da095c24d
KirtiKale0908 <kirtikale6772@gmail.com>
2026-02-02 14:14:41 +0530

    second commit

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -21,6 +21,13 @@ public class HomePage extends BasePage
   
   @FindBy(linkText = "Login")   // Login link added in step5
   WebElement linkLogin;
+  
+  @FindBy(xpath="//input[@placeholder='Search']")  //For Search Product Test
+  WebElement txtSearchbox;
+
+  @FindBy(xpath="//div[@id='search']//button[@type='button']") //For Search Product Test
+  WebElement btnSearch;
+
 
   
   public void clickMyAccount()
@@ -38,6 +45,18 @@ public class HomePage extends BasePage
   {
   	linkLogin.click();
   }
+  
+  public void enterProductName(String pName)   //For Search Product Test
+  {
+  	txtSearchbox.sendKeys(pName);
+  }
+
+  public void clickSearch()  //For Search Product Test
+  {
+  	btnSearch.click();
+  }
+
+
 
  
 }
